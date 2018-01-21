007051cf13e42ec50395ad5cfb83b266fd0855f0
Thomas Laferriere <thomas.laferriere@polymtl.ca>
2018-01-21 02:06:35 -0500

    Made first activity. Needs refinement.

---
+++ app/src/main/java/com/runloop/www/runloop/WelcomeScreenActivity.java
@@ -0,0 +1,26 @@
+package com.runloop.www.runloop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+    /** Called when the user taps the Run button */
+    public void begin(View view) {
+        Intent intent = new Intent(this, PlanRunActivity.class);
+        startActivity(intent);
+    }
+}
